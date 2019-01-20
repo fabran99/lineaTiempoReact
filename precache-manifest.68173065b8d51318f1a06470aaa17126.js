09eb391431cf2c00349ff697ce2ab36103d148a4
fabran99 <fabran99@gmail.com>
2019-01-20 03:37:55 -0300

    inicio

---
+++ precache-manifest.68173065b8d51318f1a06470aaa17126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aed3c6d7b3e7c94d487a",
+    "url": "/static/css/main.2b98ef4a.chunk.css"
+  },
+  {
+    "revision": "aed3c6d7b3e7c94d487a",
+    "url": "/static/js/main.aed3c6d7.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e2af066be2c8c22ffcd386f84e51a7e",
+    "url": "/index.html"
+  }
+];
